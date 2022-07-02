5485f2cbaca03c749a58d14ba51f3407e3e03180
Mohamed Eldamaty <damaty2000@gmail.com>
2022-07-02 17:47:54 +0200

    gulp task iti

--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,42 @@
-const gulp = require("gulp");
-const { src, dest, watch, parallel, series } = require("gulp")
-
-//minify images and copy it to dist folder
+const {src, dest} = require('gulp')
+const htmlMin = require('gulp-htmlmin')
+const concat = require('gulp-concat')
+const cssMin = require('gulp-clean-css')
+const terser = require('gulp-terser')
 const imagemin = require('gulp-imagemin');
-//don't forget to install gulp-imagemin with version 7.1.0
+// html
+function htmlTask(){    
+return src('project/*.html')
+.pipe(htmlMin({collapseWhitespace:true,removeComments:true}))
+.pipe(dest("dist"))   
+}
+exports.html = htmlTask
+
+//////////////////////////////////////////////////////////////
+
+// css 
+function cssTask(){
+    return src('project/css/**/*.css').pipe(concat("style.min.css"))
+    .pipe(cssMin()).pipe(dest("dist/assets/css"))
+}
+exports.css = cssTask
+
+/////////////////////////////////////////////////////////////
+
+//js
+function jsTask(){
+    return src("project/js/**/*.js").pipe(concat("script.min.js"))
+    .pipe(terser())
+    .pipe(dest("dist/assets/js"))
+}
+exports.js = jsTask
+
+////////////////////////////////////////////////////////////////
+
+//images
 function imgMinify() {
     return gulp.src('project/pics/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'));
+        .pipe(dest('dist/assets/images'));
 }
-
-//run image task by 'gulp' commond
-//  exports.default = imgMinify
-//  or
-//run image task by 'gulp imgMinify' commond
 exports.img = imgMinify
-
-//creating dist folder and copy html files to it
-
-const htmlmin = require('gulp-htmlmin');
-function minifyHTML() {
-    return src('project/*.html')
-        .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
-        .pipe(gulp.dest('dist'))
-}
-
-exports.html = minifyHTML
-
-
-//minify js files and copy it to dist folder
-const concat = require('gulp-concat');
-const terser = require('gulp-terser');
-
-function jsMinify() {
-    return src('project/js/**/*.js',{sourcemaps:true}) //path includeing all js files in all folders
-    
-        //concate all js files in all.min.js
-        .pipe(concat('all.min.js'))
-        //use terser to minify js files
-        .pipe(terser())
-        //create source map file in the same directory
-        .pipe(dest('dist/assets/js',{sourcemaps:'.'}))
-}
-exports.js = jsMinify
-
-
-//minify css files and copy it to dist folder
-
-var cleanCss = require('gulp-clean-css');
-function cssMinify() {
-    return src("project/css/**/*.css")
-        //concate all css files in style.min.css
-        .pipe(concat('style.min.css'))
-        //minify file 
-        .pipe(cleanCss())
-        .pipe(dest('dist/assets/css'))
-}
-exports.css = cssMinify
-//sass task
-const sass = require('gulp-sass')(require('sass'));
-function sassMinify() {
-    return src(["project/sass/**/*.scss", "project/css/**/*.css"],{sourcemaps:true})
-        .pipe(sass()) // Using gulp-sass to convert sass to css
-        //concate all js files in all.min.js
-        .pipe(concat('style.sass.min.css'))
-        .pipe(cleanCss())
-        .pipe(dest('dist/assets/css',{sourcemaps:'.'}))
-}
-
-
-
-var browserSync = require('browser-sync');
-function serve (cb){
-  browserSync({
-    server: {
-      baseDir: 'dist/'
-    }
-  });
-  cb()
-}
-
-function reloadTask(done) {
-  browserSync.reload()
-  done()
-}
-
-//watch task
-function watchTask() {
-    watch('project/*.html',series(minifyHTML, reloadTask))
-    watch('project/js/**/*.js',series(jsMinify, reloadTask))
-    watch(["project/css/**/*.css","project/sass/**/*.scss"], series(sassMinify,reloadTask));
-}
-exports.default = series( parallel(imgMinify, jsMinify, sassMinify, minifyHTML), serve,watchTask)
-
-
-
-
